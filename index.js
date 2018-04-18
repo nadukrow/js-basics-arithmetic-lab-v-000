dcbe411086f8053c2d6f85fbfd93096ddb448139
Nana Adu-Krow <nadukrow@gmail.com>
2018-04-18 04:21:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,4 +16,4 @@ const randomNumber = Math.random() + 10;
 
 const randomInteger = Math.floor(randomNumber);
 
-const randomUserID = Math.isInteger(randomInteger) + 1000000000;
+const randomUserID = randomInteger + 1000000000;
